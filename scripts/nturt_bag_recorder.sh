c3b5ff4ac52d97b9cc8ba4548faa1f574018f7b6
quantumspawner <jet22854111@gmail.com>
2023-07-12 00:33:35 +0800

    update bag naming

--- scripts/nturt_bag_recorder.sh
+++ scripts/nturt_bag_recorder.sh
@@ -3,7 +3,7 @@
 set -e
 
 # the name of the bag is the timestemp when the bag is recorded
-BAG_NAME="$(date +'%Y-%m-%d-%I-%M-%S')"
+BAG_NAME="$(date +'%Y-%m-%d-%H-%M-%S')"
 
 # start recording the bag
 ros2 bag record -o ${BAG_NAME} /from_can_bus /rosout /system_stats
